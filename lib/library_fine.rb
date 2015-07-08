c154409756f6ec99cc43d257e8843f2b049cf416
Jikku Jose <jikkujose@gmail.com>
2015-07-08 22:36:45 +0530

    Add solution for library_fine

---
+++ lib/library_fine.rb
@@ -0,0 +1,46 @@
+require 'date'
+
+class Library
+  def initialize(source: STDIN)
+    @returned_date, @due_date = parse_dates(source.read)
+  end
+
+  def parse_dates(text)
+    text.split("\n").map { |date| Date.strptime(date, '%d %m %Y') }
+  end
+
+  def fine
+    case
+    when no_dues?
+      0
+    when same_month?
+      delta_days * 15
+    when same_year?
+      delta_months * 500
+    else
+      10_000
+    end
+  end
+
+  def same_month?
+    same_year? && (@returned_date.month == @due_date.month)
+  end
+
+  def same_year?
+    @returned_date.year == @due_date.year
+  end
+
+  def delta_days
+    @returned_date.day - @due_date.day
+  end
+
+  def delta_months
+    @returned_date.month - @due_date.month
+  end
+
+  def no_dues?
+    @returned_date <= @due_date
+  end
+end
+
+puts Library.new.fine
