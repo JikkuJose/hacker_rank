3c8b3e7160cec3c14f637577c84e27dd1191bfac
Jikku Jose <jikkujose@gmail.com>
2015-07-09 17:46:37 +0530

    Add partial solution to fraud_prevention

---
+++ test/test_fraud_prevention.rb
@@ -0,0 +1,19 @@
+require_relative './test_helper'
+require 'fraud_prevention'
+
+class TestOrderAnalyzer < Minitest::Test
+  def setup
+    @given_input = DATA
+    @given_output = '1,2'
+  end
+
+  def test_given
+    assert_equal @given_output, OrderAnalyzer.new(source: @given_input).fraud_orders
+  end
+end
+
+__END__
+3
+1,1,bUgs+helo@bunny.com,123 Sesame St.,New York,NY,100-11,12345689010
+2,1,e.lm.er@fudd.com,123 Sesame St.,New York,NY,10-011,10987654321
+3,2,b.ugs@bunny.com,123 SesAMe rd.,New York,NY,1--0011,12345689010
