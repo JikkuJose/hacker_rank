1478c7573290d6e5d8a2f2c551c1b6accacb5d46
Jikku Jose <jikkujose@gmail.com>
2015-07-10 14:59:52 +0530

    Add solution for fraud_prevention

--- lib/fraud_prevention.rb
+++ lib/fraud_prevention.rb
@@ -1,5 +1,5 @@
 class Order
-  attr_reader :order_id, :deal_id, :credit_card
+  attr_reader :credit_card
 
   def initialize(row: nil)
     parse(row)
@@ -13,9 +13,16 @@ class Order
     [deal_id, city, state, zip]
   end
 
+  def order_id
+    @order_id.to_i
+  end
+
+  def deal_id
+    @deal_id.to_i
+  end
+
   def city
-    @city
-      .downcase
+    @city.downcase
   end
 
   def email
@@ -41,54 +48,48 @@ class Order
   end
 
   def zip
-    @zip
-      .gsub('-', '')
+    @zip.gsub('-', '')
   end
 
   private
 
   def parse(row)
-    data = row.strip.split(",")
-
-    @order_id = data[0]
-    @deal_id = data[1]
-    @email = data[2]
-    @street = data[3]
-    @city = data[4]
-    @state = data[5]
-    @zip = data[6]
-    @credit_card = data[7]
+    @order_id,
+      @deal_id,
+      @email,
+      @street,
+      @city,
+      @state,
+      @zip,
+      @credit_card = row.strip.split(',')
   end
 end
 
 class OrderAnalyzer
-  def initialize(source: STDIN)
+  def initialize(source: DATA)
     @input = source.readlines.drop(1)
   end
 
   def fraud_orders
-    (fraud_scheme(:scheme_one) + fraud_scheme(:scheme_two)).join(',')
+    (fraud_scheme(:scheme_one) + fraud_scheme(:scheme_two))
+      .sort
+      .uniq
+      .join(',')
   end
+
+  private
 
   def fraud_scheme(scheme)
     orders
       .group_by(&scheme)
-      .map { |_, suspects| suspects.uniq { |s| s.credit_card }.map(&:order_id) }
-      .reject { |list| list.count == 1 }
+      .map { |_, suspects| suspects.map(&:order_id) }
+      .reject { |orders| orders.count == 1 }
       .flatten
-      .sort
-      .uniq
   end
 
   def orders
-    @orders ||= @input.map { |order| Order.new(row: order) }
+    @orders ||= @input.map { |line| Order.new(row: line) }
   end
 end
 
-# puts OrderAnalyzer.new(source: DATA).fraud_orders
-
-__END__
-3
-1,1,bUgs+helo@bunny.com,123 Sesame St.,New York,NY,10011,12345689010
-2,1,e.lm.er@fudd.com,123 Sesame St.,New York,NY,10011,10987654321
-3,2,bugs@bunny.com,123 SesAMe St.,New York,NY,10011,12345689010
+# puts OrderAnalyzer.new(source: STDIN).fraud_orders
--- test/test_fraud_prevention.rb
+++ test/test_fraud_prevention.rb
@@ -3,12 +3,11 @@ require 'fraud_prevention'
 
 class TestOrderAnalyzer < Minitest::Test
   def setup
-    @given_input = DATA
     @given_output = '1,2'
   end
 
   def test_given
-    assert_equal @given_output, OrderAnalyzer.new(source: @given_input).fraud_orders
+    assert_equal @given_output, OrderAnalyzer.new.fraud_orders
   end
 end
 
@@ -17,3 +16,5 @@ __END__
 1,1,bUgs+helo@bunny.com,123 Sesame St.,New York,NY,100-11,12345689010
 2,1,e.lm.er@fudd.com,123 Sesame St.,New York,NY,10-011,10987654321
 3,2,b.ugs@bunny.com,123 SesAMe rd.,New York,NY,1--0011,12345689010
+3,22,b.ugs@bunny.com,123 SesAMe rd.,New York,CalifoRnia,1--0011,12345689010
+3,32,b.ugs@bunny.com,123 SesAMe rd.,New York,Illinois,1--0011,12345689010
