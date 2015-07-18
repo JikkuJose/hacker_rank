8ea7cc6869e372c59fd5356936eb6bf7ad166046
Jikku Jose <jikkujose@gmail.com>
2015-07-18 12:58:01 +0530

    Update solution for diagonal_difference

--- lib/diagonal_difference.rb
+++ lib/diagonal_difference.rb
@@ -1,48 +1,69 @@
-class Matrix
-  def initialize(source: STDIN)
-    @input = source.read
-    # rows
+module DiagonalDifference
+  class Matrix
+    attr_reader :rows
+
+    def initialize(rows: nil)
+      @rows = rows
+    end
+
+    def size
+      @size ||= @rows.length
+    end
+
+    def diagonal_difference
+      (sum_of_right_diagonal - sum_of_left_diagonal).abs
+    end
+
+    private
+
+    def sum_of_right_diagonal
+      right_diagonal.reduce(:+)
+    end
+
+    def sum_of_left_diagonal
+      left_diagonal.reduce(:+)
+    end
+
+    def left_diagonal
+      size.times.map { |index| rows[index][index] }
+    end
+
+    def right_diagonal
+      size.times.map { |index| rows[size - 1 - index][index] }
+    end
   end
 
-  def size
-    @size ||= lines.first.to_i
-  end
+  class HackerRank
+    def initialize(source: STDIN)
+      @input = source.read
+    end
 
-  def rows
-    @rows ||= lines[1..-1].map { |line| line.split.map(&:to_i) }
-  end
+    def result
+      Matrix.new(rows: rows).diagonal_difference.to_s
+    end
 
-  def diagonal_difference
-    (sum_of_right_diagonal - sum_of_left_diagonal).abs
-  end
+    def rows
+      @rows ||= lines.drop(1).map { |line| line.split.map(&:to_i) }
+    end
 
-  private
+    def lines
+      @input.split("\n")
+    end
 
-  def sum_of_right_diagonal
-    right_diagonal.reduce(:+)
-  end
+    def self.run
+      unless local_environment?
+        puts new.result
+      end
+    end
 
-  def sum_of_left_diagonal
-    left_diagonal.reduce(:+)
-  end
+    def self.local_environment?
+      ENV['UPSTART_JOB'].nil?
+    end
 
-  def left_diagonal
-    size.times.map { |index| rows[index][index] }
-  end
-
-  def right_diagonal
-    size.times.map { |index| rows[size - 1 - index][index] }
-  end
-
-  def lines
-    @input.split("\n")
+    def self.debug
+      puts new(source: DATA).result
+    end
   end
 end
 
-puts Matrix.new.diagonal_difference
-
-__END__
-3
-11 2 4
-4 5 6
-10 8 -12
+DiagonalDifference::HackerRank.run
