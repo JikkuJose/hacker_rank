ef5a7b9cc60ad5908a64270fdc3b3b59e51e3490
Jikku Jose <jikkujose@gmail.com>
2015-07-18 13:11:43 +0530

    Update solution for plus_minus

--- lib/plus_minus.rb
+++ lib/plus_minus.rb
@@ -1,51 +1,77 @@
-class NumberDistributions
-  def initialize(source: STDIN)
-    @input = source.read.split("\n")
+module PlusMinus
+  class NumberDistributions
+    attr_reader :array
+
+    def initialize(array: nil)
+      @array = array
+    end
+
+    def to_s
+      [
+        positive_fraction,
+        negative_fraction,
+        zero_fraction
+      ]
+        .map { |line| line.round(3) }
+        .join("\n")
+    end
+
+    def total
+      @total ||= @array.length
+    end
+
+    def postives
+      array.count { |n| n > 0 }.to_f
+    end
+
+    def negatives
+      array.count { |n| n < 0 }.to_f
+    end
+
+    def zeros
+      array.count { |n| n == 0 }.to_f
+    end
+
+    def positive_fraction
+      postives / total
+    end
+
+    def negative_fraction
+      negatives / total
+    end
+
+    def zero_fraction
+      zeros / total
+    end
   end
 
-  def result
-    [
-      positive_fraction,
-      negative_fraction,
-      zero_fraction
-    ].map { |line| line.round(3) }
-  end
+  class HackerRank
+    def initialize(source: STDIN)
+      @input = source.read.split("\n")
+    end
 
-  def total_count
-    @total_count ||= @input.first.to_i
-  end
+    def result
+      NumberDistributions.new(array: array).to_s
+    end
 
-  def array
-    @array ||= @input.last.split.map(&:to_i)
-  end
+    def array
+      @input.last.split.map(&:to_i)
+    end
 
-  def positive_count
-    array.count { |n| n > 0 }
-  end
+    def self.run
+      unless local_environment?
+        puts new.result
+      end
+    end
 
-  def negative_count
-    array.count { |n| n < 0 }
-  end
+    def self.local_environment?
+      ENV['UPSTART_JOB'].nil?
+    end
 
-  def zero_count
-    array.count { |n| n == 0 }
-  end
-
-  def positive_fraction
-    positive_count.to_f / total_count
-  end
-
-  def negative_fraction
-    negative_count.to_f / total_count
-  end
-
-  def zero_fraction
-    zero_count.to_f / total_count
+    def self.debug
+      puts new(source: DATA).result
+    end
   end
 end
 
-puts NumberDistributions.new.result
-
-__END__
-6
--4 3 -9 0 4 1
+PlusMinus::HackerRank.run
