90bcd575505b26b6e39ed27d7ecece9e2f18b063
Jikku Jose <jikkujose@gmail.com>
2015-07-18 13:21:44 +0530

    Update solution to staircase

--- lib/staircase.rb
+++ lib/staircase.rb
@@ -1,20 +1,42 @@
-class StairCase
-  SYMBOL = '#'
+module Staircase
+  class StairCaseGenerator
+    attr_reader :symbol, :height
 
-  def initialize(source: STDIN)
-    @height = source.read.to_i
+    def initialize(symbol: '#', height: 0)
+      @symbol = symbol
+      @height = height
+    end
+
+    def to_s
+      (1..height)
+        .map { |index| (symbol * index).rjust(height, ' ') }
+        .join("\n")
+    end
   end
 
-  def draw
-    (1..@height).each do |index|
-      puts (SYMBOL * index).rjust(@height, ' ')
+  class HackerRank
+    def initialize(source: STDIN)
+      @height = source.read.to_i
+    end
+
+    def result
+      StairCaseGenerator.new(height: @height).to_s
+    end
+
+    def self.run
+      unless local_environment?
+        puts new.result
+      end
+    end
+
+    def self.local_environment?
+      ENV['UPSTART_JOB'].nil?
+    end
+
+    def self.debug
+      puts new(source: DATA).result
     end
   end
 end
 
-StairCase.new.draw
-StairCase.new(source: DATA).draw
-
-__END__
-6
-
+Staircase::HackerRank.run
