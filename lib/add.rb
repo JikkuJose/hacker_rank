ffc56f39f4996e1b025ac64fe635aea64730690b
Jikku Jose <jikkujose@gmail.com>
2015-07-18 12:31:48 +0530

    Update solution for add

--- lib/add.rb
+++ lib/add.rb
@@ -1,5 +1,45 @@
-puts DATA.read.split.drop(1).map(&:to_i).reduce(:+)
+module Add
+  class List
+    def initialize(array)
+      @array = array
+    end
 
-__END__
-5
-1000000001 1000000002 1000000003 1000000004 1000000005
+    def sum
+      @array.reduce(:+)
+    end
+  end
+
+  class HackerRank
+    def initialize(source: STDIN)
+      parse(source.readlines)
+    end
+
+    def parse(lines)
+      @array = lines
+        .drop(1)
+        .first
+        .split
+        .map(&:to_i)
+    end
+
+    def result
+      List.new(@array).sum.to_s
+    end
+
+    def self.run
+      unless local_environment?
+        puts new.result
+      end
+    end
+
+    def self.local_environment?
+      ENV['UPSTART_JOB'].nil?
+    end
+
+    def self.debug
+      puts new(source: DATA).result
+    end
+  end
+end
+
+Add::HackerRank.run
